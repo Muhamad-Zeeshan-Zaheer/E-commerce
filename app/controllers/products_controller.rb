b96a16a6a7235e79a71843f2dcb1bdcc0db8c5e5
Muhammad Zeeshan Zaheer <shan6818@gmail.com>
2025-04-14 11:09:26 +0500

    changes done

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,9 +2,6 @@ class ProductsController < ApplicationController
   def index
     @products=Product.all
   end
-  def show
-    @product=Product.find(params[:id])
-  end
   def new
     @product=Product.new
   end
